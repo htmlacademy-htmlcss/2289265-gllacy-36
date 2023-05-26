d329c1581734d52250f0b8ba7788d93a10842699
psikespiegel <122777755+Alexander-Kirichenko@users.noreply.github.com>
2023-05-26 17:22:49 +0800

    slider js

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const sliderTabButtons = document.querySelectorAll('.slider-pagination-button');
+const slides = document.querySelectorAll('.slider-item');
+const  buttonPrevious = document.querySelector('.button-previous-slide');
+const  buttonNext = document.querySelector('.button-next-slide');
+let counter = 0;
+
+const clearClasses = () => {
+  document.querySelector('.current-slider').classList.remove('current-slider');
+  document.querySelector('.slider-pagination-current').classList.remove('slider-pagination-current');
+};
+
+sliderTabButtons.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    clearClasses();
+    counter = index;
+    slides[index].classList.add('current-slider');
+    tab.classList.add('slider-pagination-current');
+  });
+});
+
+buttonPrevious.addEventListener('click', () => {
+  clearClasses();
+  counter = counter -1;
+  if(counter < 0 ) {
+    counter = slides.length -1;
+  }
+
+  slides[counter].classList.add('current-slider');
+  sliderTabButtons[counter].classList.add('slider-pagination-current');
+});
+
+buttonNext.addEventListener('click', () => {
+  clearClasses();
+  counter = counter + 1;
+  if(counter > slides.length - 1) {
+    counter = 0;
+  }
+  slides[counter].classList.add('current-slider');
+  sliderTabButtons[counter].classList.add('slider-pagination-current');
+});
